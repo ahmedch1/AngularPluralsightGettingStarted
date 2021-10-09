87ffa178954fbf0b3a1cddf3ec59f15ee55ae668
Ahmed Ch <ahmedchouihi@gmail.com>
2021-10-09 13:40:36 +0100

    checklistsAndSummaryTemplatesInterpolationChapter5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
-import {Component} from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'pm-root',
   template: `
     <div>
-      <h1>{{pageTitle}}</h1>
+      <h1>{{ pageTitle }}</h1>
       <pm-products></pm-products>
-    </div>`
+    </div>
+  `,
 })
 export class AppComponent {
-  pageTitle: string = 'Acme Product management'
+  pageTitle: string = 'Acme Product Management';
 }
