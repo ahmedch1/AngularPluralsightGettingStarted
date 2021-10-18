5ca30db7bc2bdbbac101dbd1ddd291a953c5422f
Ahmed Ch <ahmedchouihi@gmail.com>
2021-10-18 18:11:43 +0100

    activatingARouteWithCode

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
 import {IProduct} from "./product";
 
 @Component({
@@ -10,7 +10,7 @@ export class ProductDetailComponent implements OnInit {
   pageTitle: string = 'Product Detail';
   product: IProduct | undefined;
 
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute, private router: Router) {
   }
 
   ngOnInit(): void {
@@ -26,6 +26,10 @@ export class ProductDetailComponent implements OnInit {
       "starRating": 3.2,
       "imageUrl": "assets/images/leaf_rake.png"
     };
+  }
+
+  onBack(): void {
+    this.router.navigate(['/products']);
   }
 
 }
