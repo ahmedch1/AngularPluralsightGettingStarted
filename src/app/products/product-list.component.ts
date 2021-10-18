7440bd2fd4579d555f9890e5e8a0d5644a810aa7
Ahmed Ch <ahmedchouihi@gmail.com>
2021-10-18 11:40:54 +0100

    'subscribingToAnObervable'

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,22 +1,22 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {IProduct} from "./product";
 import {ProductService} from "./product.service";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'pm-products',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent implements OnInit {
-  ngOnInit(): void {
-    this.products=this.productService.getProducts();
-    this.filteredProducts=this.products;
-  }
+export class ProductListComponent implements OnInit,OnDestroy {
+
 
   pageTitle: string = 'Product List';
   imageWidth: number = 50;
   imageMargin: number = 2;
   showImage: boolean = false;
+  errorMessage: string = '';
+  sub!: Subscription;
 
   private _listFilter: string = '';
   get listFilter(): string {
@@ -35,8 +35,23 @@ export class ProductListComponent implements OnInit {
   constructor(private productService: ProductService) {
   }
 
+  ngOnDestroy() {
+this.sub.unsubscribe();
+    }
+
   toggleImage(): void {
     this.showImage = !this.showImage;
+  }
+
+  ngOnInit(): void {
+    this.sub = this.productService.getProducts().subscribe({
+      next: products => {
+        this.products = products;
+        this.filteredProducts = this.products;
+      },
+      error: err => this.errorMessage = err
+    });
+
   }
 
   performFilter(filterBy: string): IProduct[] {
