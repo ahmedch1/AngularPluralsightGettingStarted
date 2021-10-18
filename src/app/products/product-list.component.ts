040cb3d02aa4a4aa5247ff5ac212262e5fb5b7e6
Ahmed Ch <ahmedchouihi@gmail.com>
2021-10-18 10:27:01 +0100

    'injectingTheService'

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {IProduct} from "./product";
+import {ProductService} from "./product.service";
 
 @Component({
   selector: 'pm-products',
@@ -8,7 +9,8 @@ import {IProduct} from "./product";
 })
 export class ProductListComponent implements OnInit {
   ngOnInit(): void {
- this.listFilter='cart';
+    this.products=this.productService.getProducts();
+    this.filteredProducts=this.products;
   }
 
   pageTitle: string = 'Product List';
@@ -21,47 +23,30 @@ export class ProductListComponent implements OnInit {
     return this._listFilter;
   }
 
-  set listFilter(value:string){
-    this._listFilter=value;
+  set listFilter(value: string) {
+    this._listFilter = value;
     console.log('In setter :', value);
-    this.filteredProducts=this.performFilter(value);
+    this.filteredProducts = this.performFilter(value);
   }
-filteredProducts:IProduct[]=[];
-  products: IProduct[] = [
-    {
-      productId: 2,
-      productName: 'Garden Cart',
-      productCode: 'GDN-0023',
-      releaseDate: 'March 18, 2021',
-      description: '15 gallon capacity rolling garden cart',
-      price: 32.99,
-      starRating: 4.2,
-      imageUrl: 'assets/images/garden_cart.png',
-    },
-    {
-      productId: 5,
-      productName: 'Hammer',
-      productCode: 'TBX-0048',
-      releaseDate: 'May 21, 2021',
-      description: 'Curved claw steel hammer',
-      price: 8.9,
-      starRating: 4.8,
-      imageUrl: 'assets/images/hammer.png',
-    },
-  ];
+
+  filteredProducts: IProduct[] = [];
+  products: IProduct[] = [];
+
+  constructor(private productService: ProductService) {
+  }
 
   toggleImage(): void {
     this.showImage = !this.showImage;
   }
 
-  private performFilter(filterBy: string):IProduct[] {
-    filterBy=filterBy.toLocaleLowerCase();
-    return this.products.filter((product:IProduct)=>
-    product.productName.toLocaleLowerCase().includes((filterBy)));
+  performFilter(filterBy: string): IProduct[] {
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.products.filter((product: IProduct) =>
+      product.productName.toLocaleLowerCase().includes((filterBy)));
   }
 
-  onRatingClicked(message: string):void {
-    this.pageTitle = 'Product List: '+ message;
+  onRatingClicked(message: string): void {
+    this.pageTitle = 'Product List: ' + message;
 
   }
 }
